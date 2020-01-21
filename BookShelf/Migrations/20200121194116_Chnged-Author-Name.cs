a670938429a95a8cc57c6c16b03ca2c7b1c1ebfb
unknown <fortunato.mugnano88@gmail.com>
2020-01-21 14:56:18 -0600

    first commit

---
+++ BookShelf/Migrations/20200121194116_Chnged-Author-Name.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BookShelf.Migrations
+{
+    public partial class ChngedAuthorName : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Author",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Name",
+                table: "Author",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+    }
+}
